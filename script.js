4519f9b3ebcd2b198ff3d03e524ef376224b12e5
Ryan Durk <rpdurk@gmail.com>
2020-08-08 14:21:31 -0700

    updated javascript to display questions

--- script.js
+++ script.js
@@ -5,35 +5,35 @@ let questions =
   [
     { question: "How do you start an HTML document?",
     answers: ["<!DOCTYPE html>", "<!docType html!>", "<DOCTTYPE HTML>","<HTML DOC>" ],
-    correctAnswer: "0",
+    correctAnswer: "<!DOCTYPE html>",
     }, {
       question: "Which of the following is an example of a closed tag",
       answers: ["<div>", "<body>", "</div>","<main>" ],
-      correctAnswer: "2",
+      correctAnswer: "</div>",
     }, {
       question: "Which element would have the largest default font size?",
     answers: ["<h4>", "<p>", "<h3>","<h2>" ],
-    correctAnswer: "3",
+    correctAnswer: "<h2>",
     }, {
       question: "What does CSS stand for?",
     answers: ["Cascading Style Sheets", "Creative Styling Solution", "Systematic Simple Styling","Creative Style Sheet" ],
-    correctAnswer: "0",
+    correctAnswer: "Cascading Style Sheets",
     }, {
       question: "When declaring a style change, what property does \"color\" alter?",
     answers: ["background color changes", "page color changes", "font color changes","color is not a css property" ],
-    correctAnswer: "2",
+    correctAnswer: "font color changes",
     }, {
       question: "If a file is labeled \"<img src=\"../picture.jpg\">\" where is the file stored?",
     answers: ["The picture.jpg file is located in the folder one level up from the current folder", "The picture.jpg file is located in the images folder at the root of the current web", "The picture.jpg file is located in the images folder in the current folder","The picture.jpg file is located in the same folder as the current page" ],
-    correctAnswer: "0",
+    correctAnswer: "The picture.jpg file is located in the folder one level up from the current folder",
     }, {
       question: "What makes \"const\" a unique variable indicator?",
     answers: ["It cannot be changed in the future", "It can be redefined later in the code", "It can be a string or a boolean","Const is not a way to indicate something is a variable" ],
-    correctAnswer: "0",
+    correctAnswer: "It cannot be changed in the future",
     }, {
       question: "What will the function math.round(x) do to a given number?",
     answers: ["round to the nearest whole number", "round up", "round down","return you an even number" ],
-    correctAnswer: "0",
+    correctAnswer: "round to the nearest whole number",
   },
 ];
 
@@ -60,6 +60,7 @@ let questions =
   let userScore = 0;
   let counter = 60;
   let currentQuestion = questions[0];
+  // let currentQuestion = 0;
 // });
 
 // Start of quiz functions
@@ -70,7 +71,7 @@ function startTimer() {
     counter--;
     $('.timer').text(counter);
     // set counter to stop at 0
-    if (counter === 0) {
+    if (counter < 0) {
       clearInterval(interval);
       endQuiz();
     } else {
@@ -89,17 +90,66 @@ function startQuestions() {
   $('.secondAnswer').text(currentQuestion.answers[1]);
   $('.thirdAnswer').text(currentQuestion.answers[2]);
   $('.fourthAnswer').text(currentQuestion.answers[3]);
-
 }
-// write an event listener for reach button and compare to answer
-$('.firstAnswer').on('click',function() {
-  if (currentQuestion.correctAnswers === );
-  points++;
-  else {
-    --TimeRanges;
+
+// write an event listener for each button and compare to correct answer of question
+// event listener
+$('.firstAnswer').on('click', function() {
+  // compare text of button to the value of correct answer
+  if ($('.firstAnswer').text() === currentQuestion.correctAnswer.valueOf()) {
+    // increase score by 100
+  userScore += 100;
+    // change button text to correct for (+) feedback
+  $('.firstAnswer').text("Correct!");
+  // empty buttons and questions
+  $('.questions').empty();
+  currentQuestion++;
+  // display next set of questions
+  startQuestions();
+    // if wrong
+} else {
+    // remove 15s from counter
+  counter -=15;
+    // give feedback for the wrong answer by changing the test
+  $('.firstAnswer').text("Wrong!");
   }
-  incrementcurrentQuestions
+  // regardless, increment the question index by 1 for the next question
+  currentQuestion ++;
 });
+
+$('.secondAnswer').on('click', function() {
+  if ($('.secondAnswer').text() === currentQuestion.correctAnswer.valueOf()) {
+  userScore + 100;
+  $('.secondAnswer').text("Correct!");
+} else {
+  counter -=15;
+  $('.secondAnswer').text("Wrong!");
+  }
+  // Need to increment currentQuestionIndex
+});
+
+$('.thirdAnswer').on('click', function() {
+  if ($('.thirdAnswer').text() === currentQuestion.correctAnswer.valueOf()) {
+  userScore + 100;
+  $('.thirdAnswer').text("Correct!");
+} else {
+  counter -=15;
+  $('.thirdAnswer').text("Wrong!");
+  }
+  // Need to increment currentQuestionIndex
+});
+
+$('.fourthAnswer').on('click', function() {
+  if ($('.fourthAnswer').text() === currentQuestion.correctAnswer.valueOf()) {
+  userScore + 100;
+  $('.fourthAnswer').text("Correct!");
+} else {
+  counter -=15;
+  $('.fourthAnswer').text("Wrong!");
+  }
+  // Need to increment currentQuestionIndex
+});
+
 
 // Start Quiz, Start Timer, Hide Start Button, Appear Questions
 $('.startBtn').on('click',function() {
@@ -112,5 +162,3 @@ $('.startBtn').on('click',function() {
   // display questions and answers
   startQuestions();
 });
-
-//  array - add items to array - save the array to local storage four loop for each item in the array - create a p tag - score = user initial values with
